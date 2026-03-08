543d1717e6c2e1d002e27bba0fa4f1dbdff62d55
sm-atahar-hossain <smataharhossain.bd@gmail.com>
2026-03-08 21:27:14 +0600

    Completed login.html

---
+++ js/login.js
@@ -0,0 +1,15 @@
+const user = document.querySelector("#userInput");
+const password = document.querySelector("#passwordInput");
+const loginButton = document.querySelector('#loginBtn');
+
+loginButton.addEventListener("click", () => {
+    if (user.value === 'admin' && password.value === 'admin123') {
+        window.location.href = "./main.html";
+    }
+    else {
+        alert("Wrong username or password");
+    }
+});
+
+
+
