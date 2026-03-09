ddba2050e6613126a3c95813d1c1b632043d6306
sm-atahar-hossain <smataharhossain.bd@gmail.com>
2026-03-09 23:09:07 +0600

    Completed Modal section

--- js/main.js
+++ js/main.js
@@ -19,7 +19,75 @@ const loadAllIssues = () => {
     .then(json => {
       allIssues = json.data;
       displayIssues(allIssues);
-    });
+    })
+    .catch(error => {
+    console.error("Search Error:", error);
+  });
+};
+const loadIssuesDetails =(id)=>{
+  fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+  .then(res=> res.json())
+  .then(json=> displayIssuesDetails(json.data))
+  .catch(error => {
+    console.error("Search Error:", error);
+  });
+}
+
+const displayIssuesDetails = (obj) => {
+  const detailsDiv = document.querySelector('#detailsDiv');
+  detailsDiv.innerHTML = `
+  <div class="w-full bg-white rounded-md flex flex-col items-start space-y-4 p-5">
+
+    <!-- Title -->
+    <div class="text-lg font-bold">${obj.title}</div>
+
+    <!-- Status & Author info -->
+    <div class="flex items-center justify-between w-full">
+     <button class="btn rounded-3xl px-4 py-1 ${ obj.status.toLowerCase() === "open" ? "btn-success text-white" : "bg-purple-500 text-white"}">${obj.status.toUpperCase()}</button>
+     <div class="text-[12px] text-gray-500 flex gap-5">
+      <span>Opened by ${obj.author}</span>
+      <span>${new Date(obj.createdAt).toLocaleDateString()}</span>
+     </div>
+    </div>
+
+    <!-- Labels -->
+    <div class="flex gap-3">${
+      !obj.labels[0] || obj.labels[0].toLowerCase() === "bug"
+      ? `
+        <button class="btn rounded-2xl bg-pink-500 text-white">BUG</button>
+        <button class="btn rounded-2xl bg-yellow-500 text-white">HELP WANTED</button>
+      `
+      : (obj.labels[0].toLowerCase() === "enhancement" || obj.labels[0].toLowerCase() === "documentation")
+        ? `<button class="btn rounded-2xl bg-green-500 text-white">${obj.labels[0].toUpperCase()}</button>`
+        : `<button class="btn rounded-2xl bg-gray-200 text-black">${obj.labels[0].toUpperCase()}</button>`
+      }
+    </div>
+
+    <!-- Description -->
+    <div class="text-sm text-gray-500">
+      ${obj.description}
+    </div>
+
+    <!-- Assignee & Priority -->
+    <div class="bg-gray-50 flex w-full justify-between p-5 rounded-lg text-sm">
+      <div class="w-1/2">
+        <p class="text-gray-500">Assignee:</p>
+        <p class="font-bold">${obj.assignee || "N/A"}</p>
+      </div>
+      <div class="flex flex-col w-1/2">
+        <p class="text-gray-500">Priority:</p>
+        <button class="btn rounded-2xl w-fit ${
+          obj.priority.toLowerCase() === "high" ? "bg-purple-500 text-white" :
+          obj.priority.toLowerCase() === "medium" ? "bg-yellow-500 text-white" :"bg-gray-500 text-white"
+        }">
+        ${obj.priority.toUpperCase()}
+        </button>
+      </div>
+    </div>
+
+  </div>
+  `;
+  document.querySelector('#my_modal').showModal();
 };
 
 const activeBtn = () => {
@@ -34,7 +102,6 @@ const activeBtn = () => {
 
 const displayIssues = (arr) => {
   let count = 0;
-  controlSpinner(true);
   const issuesDiv = document.querySelector('#issuesDiv');
   const issueCount = document.querySelector('#issuesCount');
   issuesDiv.innerHTML = '';
@@ -42,7 +109,7 @@ const displayIssues = (arr) => {
   for (let obj of arr) {
     const card = document.createElement('div');
     card.innerHTML = `
-        <div class="w-[320px] h-[420px] bg-white flex flex-col gap-4 p-10 rounded-lg ${obj.status === "open"
+        <div onclick="loadIssuesDetails(${obj.id})" class="w-[320px] h-[420px] bg-white flex flex-col gap-4 p-10 rounded-lg ${obj.status === "open"
         ? "shadow-green-600"
         : "shadow-purple-500"}  shadow-inner">
         
@@ -105,15 +172,17 @@ const filterIssues = (status) => {
   activeBtn();
 }
 
-loadAllIssues();
-
 const searchIt = () => {
   const searchInput = document.querySelector('#searchInput');
   let value = searchInput.value.trim();
-  fetch(` https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${value}`)
+  fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${value}`)
     .then(res => res.json())
     .then(json => {
       displayIssues(json.data);
-    });
+    })
+    .catch(error => {
+    console.error("Search Error:", error);
+  });
 }
+loadAllIssues();
 
